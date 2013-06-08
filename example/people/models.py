277704614a0136ee6238b343655e41880c2bc3ec
Travis Swicegood <development@domain51.com>
2013-06-08 08:14:59 +0200

    Basic models and such

--- example/people/models.py
+++ example/people/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=250)
+    code = models.CharField(max_length=2)
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.name, self.code)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=250)
+    language = models.ForeignKey(Language, related_name='people')
+
+    def __unicode__(self):
+        return self.name
